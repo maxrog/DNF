96f4c69514bb6e47f6319f3b9196a5571c83a33d
Max Rogers <max.rogers@yinzcam.com>
2022-06-08 20:59:23 -0400

    add basic lifcycle boilerplate

--- DNF/DNF/DNFApp.swift
+++ DNF/DNF/DNFApp.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 @main
 struct DNFApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+    
+    init() {
+        /// perform and work necessary on app launch
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    /// handle deeplink url
+                }
+        }
+        .onChange(of: scenePhase) { newScenePhase in
+            switch scenePhase {
+            case .active:
+                /// application became active
+                break
+            case .background:
+                /// application went to background
+                break
+            case .inactive:
+                /// application became inactive
+                break
+            default:
+                break
+            }
         }
     }
 }
