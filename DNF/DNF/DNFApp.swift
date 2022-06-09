f281f97f2222faf2255d18867c54044b5b9a3ea8
Max Rogers <max.rogers@yinzcam.com>
2022-06-08 20:27:43 -0400

    Create Xcode Project

---
+++ DNF/DNF/DNFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNFApp.swift
+//  DNF
+//
+//  Created by Max Rogers on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DNFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
