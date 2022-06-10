1c96814c32b5ce8fde34d65be1f5f0d7d4f4553c
Max Rogers <max.rogers@yinzcam.com>
2022-06-10 07:39:28 -0400

    basic auth manager

---
+++ DNF/DNF/Authentication/AuthenticationManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationManager.swift
+//  DNF
+//
+//  Created by Max Rogers on 6/10/22.
+//
+
+import Foundation
+
+/*
+ Manager for maintaining auth state
+ */
+
+class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    private init() {}
+}
