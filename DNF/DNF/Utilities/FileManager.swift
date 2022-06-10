f8bc2efdb7f7f61703f37c4ef31cb5b9874fe68e
Max Rogers <max.rogers@yinzcam.com>
2022-06-10 07:39:15 -0400

    refactor plist parsing

--- DNF/DNF/Utilities/FileManager.swift
+++ DNF/DNF/Utilities/FileManager.swift
@@ -7,28 +7,26 @@
 
 import Foundation
 
-class FileManager {
+struct FileManager {
     
     /// fetch a plist to read from
-    static func plist(named name: String, childName: String? = nil) -> [String : AnyHashable] {
-        let plistNotFound: [String : AnyHashable] = [:]
+    static func plist(named name: String, childName: String? = nil) -> [String : AnyObject] {
+        let plistNotFound: [String : AnyObject] = [:]
+        
         guard let path = Bundle.main.path(forResource: name, ofType: "plist") else {
             return plistNotFound
         }
-        let url = URL(fileURLWithPath: path)
-        guard let data = try? Data(contentsOf: url) else {
-            return plistNotFound
-        }
-        guard let plist = try? PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as? [String:AnyHashable] else {
+    
+        guard let rootPlist = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> else {
             return plistNotFound
         }
         
         // if we want to drill down into the plist
-        if let childName = childName {
-            return plist[childName] as? [String : AnyHashable] ?? plistNotFound
+        if let childName {
+            return rootPlist[childName] as? [String : AnyObject] ?? plistNotFound
         }
         
-        return plist
+        return rootPlist
     }
     
 }
