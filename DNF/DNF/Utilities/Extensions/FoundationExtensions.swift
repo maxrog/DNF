ffc07bb4c87f658402533b99099544ddd83ee248
Max Rogers <max.rogers@yinzcam.com>
2022-06-11 06:46:38 -0400

    add query parameter subscript

--- DNF/DNF/Utilities/Extensions/FoundationExtensions.swift
+++ DNF/DNF/Utilities/Extensions/FoundationExtensions.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+extension URL {
+    subscript(queryParam: String) -> String? {
+        guard let url = URLComponents(string: self.absoluteString) else { return nil }
+        return url.queryItems?.first(where: { $0.name == queryParam })?.value
+    }
+}
